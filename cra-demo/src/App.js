2ed9ae59302e4ee53eec39748f2855d6665ee8bf
yw.hao <yw.hao@trip.com>
2024-02-21 20:14:02 +0800

    feat: add bootstrap

--- cra-demo/src/App.js
+++ cra-demo/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* bootstrap引入测试 */}
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">
+          Example textarea
+        </label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
     </div>
   );
 }
